2c0dbd749c506436c7b989ad807911e2c11e64d9
Tom Coleman <tom@thesnail.org>
2011-10-18 17:09:55 +1100

    Committed sass files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SassCacheProblems::Application.config.secret_token = 'cbc8836af229cef8b3ee9112a51423463adee7e579336e15cc83e9afaa141af266085934f3a862bd10bc40d21b1fdf811be69caa60658e6af4f29692cc1705ea'
